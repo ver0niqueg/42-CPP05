b1278c5c2cc96b5e9bc21cbe47a03c2c8e51a902
Veronique Galmiche <vgalmich@f4r5s16.clusters.42paris.fr>
2025-09-13 14:30:29 +0200

    OK

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vgalmich <vgalmich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/03 15:06:30 by vgalmich          #+#    #+#             */
-/*   Updated: 2025/09/10 18:29:26 by vgalmich         ###   ########.fr       */
+/*   Updated: 2025/09/13 14:26:43 by vgalmich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int main()
 	try 
 	{
 		Bureaucrat a("Vero", 2);
-		std::cout << a<< std::endl;
+		std::cout << a << std::endl;
 
 		a.incrementGrade();
 		std::cout << "After grade up: " << a << std::endl;
